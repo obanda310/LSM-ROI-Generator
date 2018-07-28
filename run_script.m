5cea6cdab21de1d437ef38a5f5ddcff7ebd4df0e
obanda310 <obanda310@gmail.com>
2018-07-28 11:12:44 -0400

    Adds a GUI for selecting options
    Should be more user-friendly

--- run_script.m
+++ run_script.m
@@ -26,39 +26,10 @@ pixelsize = input('What is the pixel size in microns?');
 finalRes(1,1) = input('What is the final X resolution in pixels (desired frame size in ZEN)?');
 finalRes(1,2) = input('What is the final Y resolution in pixels (desired frame size in ZEN)?');
 tic
-%% Add other options here:
-%Remove small regions?
-options(1,1) = 0; %feature on or off (1/0)
 
-%Convert single pixels to triangles
-options(2,1) = 0; %feature on or off (1/0)
+%% Run the options selection window
 
-%Convert single pixels to squares
-options(5,1) = 1; %feature on or off (1/0)
-if options(5,1)==1
-    options(2,1)=0;
-end
-
-%Run version 2 of Mask2Poly
-options(3,1) = 0; %feature on or off (1/0)
-
-%Divide image horizontally to break open stuctures
-options(4,1) = 1; %feature on or off (1/0)
-
-%Open images stack and compress images into single file. This can be useful
-%to create many negative spaces with fewer features. Not compatible with 3D
-%regions.
-options(6,1) = 1; %feature on or off (1/0)
-if options(6,1) == 1
-    options(4,1) = 0;
-end
-
-%Reduce the number of rendered outputs for assessing quality. (Useful for
-%large images stacks with more than ~20 images in the stack)
-options(7,1) = 1;
-
-%%Use full pixel trace (no shortcuts on any corners)
-options(9,1) = 0;
+options = ROIGenOpt();
 
 %% Iterative .Regions and .ovl outputs
 if options(6,1) == 0
